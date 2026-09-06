b4d9b6a85cd22299634579fe2a4ecae9bbdd748e
Alejandro Amaral <aamaral@logico.com.ar>
2026-09-06 14:41:14 -0300

    fix: telescope themes

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,26 +1,29 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  lazy = true,
-  cmd = 'Telescope',
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-ui-select.nvim",
-    "fdschmidt93/telescope-egrepify.nvim",
-    "andrew-george/telescope-themes",
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  },
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = {
-        height = 0.65,
-        width = 0.75,
-      },
+    "nvim-telescope/telescope.nvim",
+    lazy = true,
+    cmd = 'Telescope',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim",
+        "fdschmidt93/telescope-egrepify.nvim",
+        "andrew-george/telescope-themes",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    extensions = {
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown({}),
-      },
-    },
-  },
+    opts = function()
+        local themes = require("telescope.themes")
+        return {
+            defaults = {
+                layout_strategy = "horizontal",
+                layout_config = {
+                    height = 0.65,
+                    width = 0.75,
+                },
+            },
+            extensions = {
+                ["ui-select"] = {
+                    themes.get_dropdown({}),
+                },
+            },
+        }
+    end
 }
